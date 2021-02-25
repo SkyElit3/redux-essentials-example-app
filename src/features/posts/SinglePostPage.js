a6c175b8ed69f0cc37a062ffaefeea7ec1701fc2
Pascanu Stefan <virgil_stefan2000@yahoo.com>
2021-02-25 13:32:22 +0200

    chapter 5 - added custom selectors

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,11 +4,12 @@ import {Link} from 'react-router-dom'
 import {PostAuthor} from "./PostAuthor";
 import {TimeAgo} from "./TimeAgo";
 import {ReactionButtons} from "./ReactionButtons";
+import {selectPostById} from "./postsSlice";
 
 export const SinglePostPage = ({match}) => {
     const {postId} = match.params
 
-    const post = useSelector(state => state.posts.find(post => post.id === postId))
+    const post = useSelector(state => selectPostById(state,postId))
     // the component will re-render any time the value returned from useSelector changes to a new reference
 
     if (!post) {
